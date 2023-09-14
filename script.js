59aff7402b27878a3974ee4ee7a97c5fee3251e8
Abhilash4597 <abhilash4597@gmail.com>
2023-09-15 01:04:56 +0530

    some function added

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 let panel_bottom = document.querySelector('#panel_btm');
-
+let timerVal = document.querySelector('#timerVal');
+let hitVal = document.querySelector('#hitVal');
+let scoreVal = document.querySelector('#scoreVal');
 let bubbles = '';
-// let randomNumber = 0;
+let time = 60;
+let randomHit =0; 
 
-for (let i = 1; i <= 150; i++){
-  let randomNumber = Math.floor(Math.random()*10)
-  bubbles += `<div class="bubble">${randomNumber}</div>`;
+function makeBubble() {
+  for (let i = 1; i <= 150; i++) {
+    let randomNumber = Math.floor(Math.random() * 10);
+    bubbles += `<div class="bubble">${randomNumber}</div>`;
+  }
+
+  panel_bottom.innerHTML = bubbles;
 }
 
-panel_bottom.innerHTML = bubbles;
+function runningTimer() {
+  let stopTimer = setInterval(() => {
+    if (time > 0) {
+      time--;
+      timerVal.textContent = time;
+    } else {
+      clearInterval(stopTimer)
+    }
+  }, 1000);
+}
+
+function getNewHit() {
+  randomHit = Math.floor(Math.random() * 10);
+  hitVal.textContent = randomHit;
+}
+
+makeBubble();
+runningTimer();
+getNewHit();
+
+
