8b2d9c43eecae33d420a12f5e86d111082f8d972
Abhilash4597 <abhilash4597@gmail.com>
2023-09-14 19:24:34 +0530

    js numbers added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+let panel_bottom = document.querySelector('#panel_btm');
+
+let bubbles = '';
+// let randomNumber = 0;
+
+for (let i = 1; i <= 150; i++){
+  let randomNumber = Math.floor(Math.random()*10)
+  bubbles += `<div class="bubble">${randomNumber}</div>`;
+}
+
+panel_bottom.innerHTML = bubbles;
